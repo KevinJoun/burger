a0d106924f1c4f7692424d7356f6b5f27138396f
SilverBell17 <kevinjoun17@gmail.com>
2019-11-11 14:44:51 -0800

    adding code

---
+++ models/burger.js
@@ -0,0 +1,17 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        })
+    },
+    insertOne: function(vals,cb){
+        orm.insertOne("burgers", "burger_name", vals, function(res){
+            cb(res)
+        })
+    },
+    updateOne: function(objColVals, condition, cb){
+        
+    }
+}
